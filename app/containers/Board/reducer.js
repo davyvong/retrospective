e6840205fac10b37e7f394459fefa8a57e563acb
Davy Vong <davy.vong@gmail.com>
2019-04-06 17:21:50 -0400

    Add reducer cases for modified and deleted board items

--- app/containers/Board/reducer.js
+++ app/containers/Board/reducer.js
@@ -16,26 +16,56 @@ export const initialState = fromJS({
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case BOARD_ITEM_ADDED: {
-      if (isType(action.params, 'Object')) {
-        const { params: doc } = action;
-        if (
-          isType(doc, 'Object') &&
-          isType(doc.data, 'Object') &&
-          isGUID(doc.id, 'String')
-        ) {
-          return state.setIn(['items', doc.id], doc.data);
-        }
-      }
-      return state;
-    }
+    case BOARD_ITEM_ADDED:
+      return boardItemAdded(state, action);
     case BOARD_ITEM_MODIFIED:
-      return state;
+      return boardItemModified(state, action);
     case BOARD_ITEM_DELETED:
-      return state;
+      return boardItemDeleted(state, action);
     default:
       return state;
   }
+}
+
+function boardItemAdded(state, action) {
+  if (isType(action.params, 'Object')) {
+    const { params: doc } = action;
+    if (
+      isType(doc, 'Object') &&
+      isType(doc.data, 'Object') &&
+      isGUID(doc.id, 'String')
+    ) {
+      return state.setIn(['items', doc.id], doc.data);
+    }
+  }
+  return state;
+}
+
+function boardItemModified(state, action) {
+  if (isType(action.params, 'Object')) {
+    const { params: doc } = action;
+    if (
+      isType(doc, 'Object') &&
+      isType(doc.data, 'Object') &&
+      isGUID(doc.id, 'String')
+    ) {
+      return state.updateIn(['items', doc.id], value => ({
+        ...value,
+        ...doc.data,
+      }));
+    }
+  }
+  return state;
+}
+
+function boardItemDeleted(state, action) {
+  if (isType(action.params, 'Object')) {
+    const { params: doc } = action;
+    if (isType(doc, 'Object') && isGUID(doc.id, 'String')) {
+      return state.get('items').delete(doc.id);
+    }
+  }
+  return state;
 }
 
 export default reducer;
