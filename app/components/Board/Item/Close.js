a5e43bf2cfdf420000b51d658df90478013e21c2
Davy Vong <davy.vong@gmail.com>
2019-04-11 22:04:59 -0400

    Increase size of Delete and Close comp

--- app/components/Board/Item/Close.js
+++ app/components/Board/Item/Close.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 const Component = styled.i.attrs({ className: 'material-icons' })`
   background-color: #4a4a4a;
-  border-radius: 12px;
+  border-radius: 14px;
   color: #ffffff;
   display: flex;
   flex-direction: column;
   font-size: 16px;
-  height: 24px;
+  height: 28px;
   justify-content: center;
   opacity: 1 !important;
   position: absolute;
@@ -15,7 +15,7 @@ const Component = styled.i.attrs({ className: 'material-icons' })`
   text-align: center;
   top: -10px;
   visibility: visible !important;
-  width: 24px;
+  width: 28px;
 `;
 
 export default Component;
--- app/components/Board/Item/Delete.js
+++ app/components/Board/Item/Delete.js
@@ -4,19 +4,19 @@ import { COLORS } from 'constants/colors';
 
 const Component = styled.i.attrs({ className: 'material-icons' })`
   background-color: ${COLORS.RED};
-  border-radius: 12px;
+  border-radius: 14px;
   color: #ffffff;
   display: flex;
   flex-direction: column;
   font-size: 16px;
-  height: 24px;
+  height: 28px;
   justify-content: center;
   position: absolute;
   right: -10px;
   text-align: center;
   top: -10px;
   transition: visibility 0s, opacity 0.3s linear;
-  width: 24px;
+  width: 28px;
 `;
 
 export default Component;
