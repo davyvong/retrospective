5ba4d6dd14d0d67c989364aae1c276a57b5789fb
Davy Vong <davy.vong@intelex.com>
2019-04-15 16:30:27 -0400

    Add item delete trigger to delete comments

--- functions/src/app.js
+++ functions/src/app.js
@@ -9,12 +9,24 @@ exports.onDeleteGroup = functions.firestore
   .document('boards/{boardId}/groups/{groupId}')
   .onDelete((snap, context) => {
     const { boardId, groupId } = context.params;
-    return firestore
-      .collection(`boards/${boardId}/items`)
-      .where('parent', '==', groupId)
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => doc.ref.delete());
-        return 0;
-      });
+    return Promise.all([
+      firestore
+        .collection(`boards/${boardId}/items`)
+        .where('parent', '==', groupId)
+        .get()
+        .then(querySnapshot => querySnapshot.forEach(doc => doc.ref.delete())),
+    ]).then(() => 0);
+  });
+
+exports.onDeleteItem = functions.firestore
+  .document('boards/{boardId}/items/{itemId}')
+  .onDelete((snap, context) => {
+    const { boardId, itemId } = context.params;
+    return Promise.all([
+      firestore
+        .collection(`boards/${boardId}/comments`)
+        .where('parent', '==', itemId)
+        .get()
+        .then(querySnapshot => querySnapshot.forEach(doc => doc.ref.delete())),
+    ]).then(() => 0);
   });
