805a2c10cf31b8ff5d549571de8004c2746c77a6
Davy Vong <24511732+davyvong@users.noreply.github.com>
2019-06-18 16:57:49 -0400

    Resolve improper textbox height

--- hosting/app/components/Board/Item/Message.js
+++ hosting/app/components/Board/Item/Message.js
@@ -3,8 +3,6 @@ import TextareaAutosize from 'react-autosize-textarea';
 
 const Component = styled(TextareaAutosize)`
   border-radius: 6px;
-  display: flex;
-  flex: 1;
   line-height: 1.5;
   margin-bottom: 0.5rem !important;
   padding: 0.25rem 0.5rem;
